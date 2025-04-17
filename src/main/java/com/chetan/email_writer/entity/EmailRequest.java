939d88fa8964884f4fddc248af3d4766bdfc2191
Chetan Dongare <150001627+selenophile1805@users.noreply.github.com>
2025-04-17 16:11:27 +0530

    Updated EmailRequest.java

--- src/main/java/com/chetan/email_writer/entity/EmailRequest.java
+++ src/main/java/com/chetan/email_writer/entity/EmailRequest.java
@@ -1,31 +1,15 @@
 package com.chetan.email_writer.entity;
 
-
-import lombok.*;
-import org.springframework.stereotype.Service;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 public class EmailRequest {
-    public String getEmailContent() {
-        return emailContent;
-    }
-
-    public void setEmailContent(String emailContent) {
-        this.emailContent = emailContent;
-    }
-
-    public String getTone() {
-        return tone;
-    }
-
-    public void setTone(String tone) {
-        this.tone = tone;
-    }
-
     private String emailContent;
     private String tone;
 }
