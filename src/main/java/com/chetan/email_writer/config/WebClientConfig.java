c1ae7879d1217aa31a4f19c15466b8b3e8e3077b
Chetan Dongare <150001627+selenophile1805@users.noreply.github.com>
2025-04-17 12:39:29 +0530

    Updated WebClientConfig.java

--- src/main/java/com/chetan/email_writer/config/WebClientConfig.java
+++ src/main/java/com/chetan/email_writer/config/WebClientConfig.java
@@ -1,17 +1,16 @@
 package com.chetan.email_writer.config;
 
-import lombok.Data;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-@Data
 public class WebClientConfig {
 
     @Bean
     public WebClient webClient() {
-
-        return WebClient.builder().build();
+        return WebClient.builder()
+                .baseUrl("https://generativelanguage.googleapis.com")
+                .build();
     }
 }
